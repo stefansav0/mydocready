f9bcdcb0e3b27e492d63408eeadd39d0ea4250e6
stefansav0 <ravikumar.kumar2333@gmail.com>
2025-09-01 12:19:30 +0530

    fixed postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,5 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
 const config = {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
